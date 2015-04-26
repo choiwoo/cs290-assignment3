37d62fd7e7138dd7c09a051eeeaccaab1880a491
Woo Choi <choiwoo@onid.oregonstate.edu>
2015-04-25 22:15:03 -0700

    Finished Object.js, last one was function.js haha

--- src/objects.js
+++ src/objects.js
@@ -10,7 +10,13 @@
 
 function returnObjectLiteral() {
   //your code here
-  return undefined; //Modify ONLY this line
+  var object = new Object();
+  object.type = 'Goldfish';
+  object.brand = 'Pepperidge Farm';
+  object.flavor = 'Cheddar';
+  object.count = 2000;
+ 
+  return object; //Modify ONLY this line
   //end your code
 }
 
@@ -38,7 +44,47 @@ function returnObjectLiteral() {
 */
 
 //your code here
+function MessageLog(user){
+  this.user = user;
+  this.mSent = [];
+  this.mRec = [];
+  this.countSent = 0;
+  this.countRec = 0;
+  this.logMessage = function(messageText, direction){
+	  if (direction == 0){
+		this.countSent++;
+		this.mSent.push(messageText);
+	  }
 
+	  if (direction == 1)  
+	  {
+		this.countRec++;
+		this.mRec.push(messageText);
+	  }
+	  if (this.mSent.length > 5) {
+        this.mSent.splice(0, 1);
+            
+      }
+	  else 
+	    return undefined; //behavior for other number is undefined
+  };
+  //get sent message
+  this.getSentMessage = function(n){
+	if (n>= 0 && n <5){
+	  return this.mSent[4-n];
+	}
+	else
+	  console.log('0~4 only');
+  };
+  //return msgs sent
+  this.totalSent = function(){
+	return this.countSent;
+  };
+  //return msgs received
+  this.totalReceived = function(){
+	return this.countRec;
+  };
+}
 //end your code
 
 /**
@@ -47,7 +93,11 @@ function returnObjectLiteral() {
 * received.
 */
 //your code here
+MessageLog.prototype.lastReceivedMessage = function(){
 
+  return this.mRec[this.mRec.length - 1];
+
+};
 //end your code
 
 /**
@@ -57,5 +107,9 @@ function returnObjectLiteral() {
 */
 
 //your code here
+var myLog = new MessageLog("BlackHatGuy");
+  myLog.logMessage("foo", 1);
+  myLog.logMessage("bar", 1);
+  myLog.logMessage("baz", 1);
 
 //end your code
